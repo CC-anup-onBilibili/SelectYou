eaff5935c8d04d9eb1cd27336589874f58aa7898
programmerCC <18770248413@163.com>
2025-11-01 12:09:11 +0800

    hello

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import PySide6.QtWidgets as qw
+import PySide6.QtCore as qc
+import PySide6.QtGui as qg
+import qfluentwidgets as qfw
+import sys
+
+app = qw.QApplication(sys.argv)
+window = qfw.FluentWindow()
